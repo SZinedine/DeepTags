9458d7ef100a8223ea8bd7c49347eded5c0c2741
SZinedine <saibi.zineddine@yahoo.com>
2020-04-06 17:59:31 +0100

    added help message and version argument

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <QLibraryInfo>
 #include <QTranslator>
 #include "gui/mainwindow.h"
+#include <cstring>
 
 #define VERSION "0.5"
 #define NAME    "DeepTags"
@@ -14,7 +15,27 @@
 #define _QAPP QApplication
 #endif
 
+#define HELP_MESSAGE \
+    "DeepTags " VERSION "\n" \
+    "Copyright (C) 2020 Zineddine SAIBI <saibi.zineddine@yahoo.com>.\n"\
+    "License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>\n"\
+    "This is free software: you are free to change and redistribute it."
+
 int main(int argc, char* argv[]) {
+    if (argc == 2) {
+        if (std::strcmp(argv[1], "-v") == 0 || std::strcmp(argv[1], "--version") == 0) {
+            std::cout << VERSION << std::endl;
+            return 0;
+        }
+        else if (std::strcmp(argv[1], "-h") == 0 || std::strcmp(argv[1], "--help") == 0) {
+            std::cout << HELP_MESSAGE << std::endl;
+            return 0;
+        }
+        else {
+            std::cout << "ERROR. Try to run DeepTags without any argument." << std::endl;
+            return 1;
+        }
+    }
     _QAPP app(argc, argv);
     app.setApplicationName(NAME);
     app.setApplicationVersion(VERSION);
