46399943f3a7cad258f71c65c56819a30a44116c
SZinedine <saibi.zineddine@yahoo.com>
2020-04-06 22:53:50 +0100

    inhanced CI config files and packaging scripts

--- packaging/appimage.sh
+++ packaging/appimage.sh
@@ -1,45 +1,71 @@
 #!/bin/bash
 
-_APP_VERSION=0.5
 _APP_NAME=DeepTags
-_PKG_DIR=$(realpath $(dirname $0))
-_ROOT=$(realpath $_PKG_DIR/..)
-_DESKTOP_FILE=$_ROOT/$_APP_NAME.desktop
-_ICON_FILE=$_ROOT/$_APP_NAME.png
-_TRANSLATIONS=$_ROOT/locale/*.qm
+_PKG_DIR=$(readlink -f $(dirname $0))
+_ROOT=$(readlink -f $_PKG_DIR/..)
 _APP_BIN=$_ROOT/build/release/$_APP_NAME
+_RC_DIR=$_PKG_DIR/resources
+_DESKTOP_FILE=$_RC_DIR/$_APP_NAME.desktop
+_ICON_FILE=$_ROOT/$_APP_NAME.png
+_ICON_DIR=$_RC_DIR/icons
+_TRANSLATIONS=$_ROOT/locale/*.qm
+_APP_VERSION=$($_APP_BIN --version)
 _APPIMAGE_DIR=$_PKG_DIR/deeptags_"$_APP_VERSION"
 _OUPUT="$_APPIMAGE_DIR"-86_x64.AppImage
 _LINUXDEPLOYQT="$_PKG_DIR"/linuxdeployqt-*-x86_64.AppImage
+export VERSION=$_APP_VERSION    # used by linuxdeployqt
 
 rm -rf $_APPIMAGE_DIR
 
 # Check if we are on Linux
 if [ "$(uname)" != "Linux" ]; then
-    echo "This script only works on linux."
+    >&2 echo "This script only works on linux."
     exit 1
 fi
 
 if [ ! -f "$_APP_BIN" ]; then
-    echo "the binary of $_APP_NAME doesn't exist."
+    >&2 echo "the binary of $_APP_NAME doesn't exist."
     exit 1
 fi
 
+# download linuxdeployqt if it doesn't exist
+if [ ! -f $_LINUXDEPLOYQT ]; then
+    >&2 echo "LinuxDeployQt doesn't exit."
+    echo "Downloading LinuxDeployQt..."
+    wget -P $_PKG_DIR -c https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage
+    chmod +x $_LINUXDEPLOYQT
+fi
+
+
+echo "creating the directory structure"
 mkdir -p $_APPIMAGE_DIR/usr/bin
 mkdir -p $_APPIMAGE_DIR/usr/translations
 mkdir -p $_APPIMAGE_DIR/usr/share/applications
-mkdir -p $_APPIMAGE_DIR/usr/share/icons/hicolor/256x256/apps/
+
 
 echo "copying necessary files"
-cp $_DESKTOP_FILE $_APPIMAGE_DIR/usr/share/applications
-cp $_ICON_FILE $_APPIMAGE_DIR/usr/share/icons/hicolor/256x256/apps/
-cp $_DESKTOP_FILE $_APPIMAGE_DIR/
-cp $_ICON_FILE $_APPIMAGE_DIR/
-cp $_TRANSLATIONS $_APPIMAGE_DIR/usr/translations/
-cp $_APP_BIN $_APPIMAGE_DIR/usr/bin
+cp $_DESKTOP_FILE   $_APPIMAGE_DIR/
+cp $_DESKTOP_FILE   $_APPIMAGE_DIR/usr/share/applications
+cp -r $_ICON_DIR    $_APPIMAGE_DIR/usr/share/
+cp $_ICON_FILE      $_APPIMAGE_DIR/
+cp $_TRANSLATIONS   $_APPIMAGE_DIR/usr/translations/
+cp $_APP_BIN        $_APPIMAGE_DIR/usr/bin
 
-echo "creaeting the appimage"
-$_LINUXDEPLOYQT $_APPIMAGE_DIR/usr/bin/$_APP_NAME -appimage -bundle-non-qt-libs
+
+# if --add-libstdc++ is provided, bundle libstdc++.so.x 
+if [ "$*" == "--add-libstdc++" ]; then
+    echo "copying libstdc++"
+    mkdir -p $_APPIMAGE_DIR/usr/lib
+    cp /usr/lib/x86_64-linux-gnu/libstdc++.so* $_APPIMAGE_DIR/usr/lib/
+fi
+
+
+echo "creating the appimage"
+RESULT=$($_LINUXDEPLOYQT $_APPIMAGE_DIR/usr/bin/$_APP_NAME -appimage -bundle-non-qt-libs)
+
+if [ $? != 0 ]; then
+    >&2 echo "ERROR. LinuxDeployQt failed."
+fi
 
 
 echo "deleting temporary files"
--- packaging/deb.sh
+++ packaging/deb.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-_APP_VERSION=0.5
 _APP_NAME=DeepTags
-_PKG_DIR=$(realpath $(dirname $0))
-_ROOT=$(realpath $_PKG_DIR/..)
-_DESKTOP_FILE=$_ROOT/$_APP_NAME.desktop
-_ICON_FILE=$_ROOT/$_APP_NAME.png
+_PKG_DIR=$(readlink -f $(dirname $0))
+_ROOT=$(readlink -f $_PKG_DIR/..)
 _APP_BIN=$_ROOT/build/release/$_APP_NAME
+_RC_DIR=$_PKG_DIR/resources
+_DESKTOP_FILE=$_RC_DIR/$_APP_NAME.desktop
+_ICON_FILE=$_ROOT/$_APP_NAME.png
+_ICON_DIR=$_RC_DIR/icons
+_TRANSLATIONS=$_ROOT/locale/*.qm
+_APP_VERSION=$($_APP_BIN --version)
 _DEB_DIR=$_PKG_DIR/deeptags_"$_APP_VERSION"_amd64
+
+
+rm -rf $_DEB_DIR
 
 
 # Check if we are on Linux
@@ -21,11 +27,18 @@ if [ ! -f "$_APP_BIN" ]; then
     exit 1
 fi
 
+
 echo "creating directories for packaging"
 mkdir -p $_DEB_DIR/DEBIAN
 mkdir -p $_DEB_DIR/usr/bin
 mkdir -p $_DEB_DIR/usr/share/applications
-mkdir -p $_DEB_DIR/usr/share/icons/hicolor/256x256/apps/
+
+
+echo "copying necessary files"
+cp $_DESKTOP_FILE   $_DEB_DIR/usr/share/applications
+cp -r $_ICON_DIR    $_DEB_DIR/usr/share/
+cp $_APP_BIN        $_DEB_DIR/usr/bin
+
 
 echo "creating control file"
 cat >> $_DEB_DIR/DEBIAN/control <<EOL
@@ -35,20 +48,16 @@ Architecture: amd64
 Essential: no
 Priority: optional
 Section: utils
-Depends: libqt5widgets5 (>= 5.6), libqt5gui5 (>= 5.6), libqt5network5 (>= 5.6), libqt5core5a (>= 5.6), libc6, libgcc1, libstdc++6, libx11-6
+Depends: libqt5widgets5 (>= 5.6), libqt5gui5 (>= 5.6), libqt5network5 (>= 5.6), libqt5core5a (>= 5.6), libc6, libgcc1, libstdc++6, libx11-6, libgl1-mesa-dev
 Maintainer: Zineddine SAIBI <saibi.zineddine@yahoo.com>
 Homepage: https://www.github.com/SZinedine/DeepTags
 Description: A Markdown notes manager
 EOL
 
-echo "copying necessary files"
-cp $_DESKTOP_FILE $_DEB_DIR/usr/share/applications
-cp $_ICON_FILE $_DEB_DIR/usr/share/icons/hicolor/256x256/apps/
-cp $_APP_BIN $_DEB_DIR/usr/bin
-
 
 echo "creating the deb package"
 dpkg-deb --build $_DEB_DIR
 
+
 echo "deleting temporary files"
-rm -rf _DEB_DIR
+rm -rf $_DEB_DIR
